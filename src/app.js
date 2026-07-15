619fc6b39d323b50526d7c99302369415ce725b7
Ali2006-NED <aliahmed23022006@gmail.com>
2026-07-15 17:24:06 +0500

    Stage 1: root and health endpoints

--- src/app.js
+++ src/app.js
@@ -4,8 +4,16 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/hello', (req, res) => {
+  res.send({ "message": "Hello, World!" });
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] });
+});
+
+app.get('/health', (req, res) => {
+  res.send({ "status": "ok" });
 });
 
 module.exports = app;
