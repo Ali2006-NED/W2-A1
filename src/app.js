a863fe2220eda01a93b948c9995443c3a6a494ed
Ali2006-NED <aliahmed23022006@gmail.com>
2026-07-20 20:48:07 +0500

    Stage 2: read endpoints with 404

--- src/app.js
+++ src/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
+const tasks = [{"id":1,"title":"Task 1","done":false},{"id":2,"title":"Task 2","done":true},{"id":3,"title":"Task 3","done":false}];
+
 app.use(express.json());
 
 app.get('/hello', (req, res) => {
   res.send({ "message": "Hello, World!" });
 });
 
-app.get('/', (req, res) => {
-  res.send({ "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] });
+app.get('/tasks', (req, res) => {
+  res.send({ "tasks": tasks });
+});
+app.get('/tasks/:id',(req,res) => {
+  const id = req.params.id;
+  if (tasks.some(tasks => tasks.id == id)){
+    res.send({"task":tasks[id-1]});
+  } else {
+    res.status(404).send({"error":`Task ${id} not found`});
+  }
 });
 
 app.get('/health', (req, res) => {
